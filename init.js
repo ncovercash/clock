ffcd5466b7f3ed222578a72f4e8479ada04cb092
Noah Overcash <ncovercash@crimson.ua.edu>
2020-03-21 02:53:04 -0400

    Update jQuery and a few minor fixes from copying

--- init.js
+++ init.js
@@ -1,1 +1,8 @@
+// this needs to happen first for some reason
+var today = new Date();
+document.getElementById("txt2").innerText = 
+	days[today.getDay()] + ", " +
+	month[today.getMonth()] + " " +
+	today.getDate();
+
 renderTime();
